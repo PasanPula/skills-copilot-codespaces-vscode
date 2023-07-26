0cc6133436e127e622c1117c8bb9fdc25a00ab49
Pasan Pulasithi <61830206+PasanPula@users.noreply.github.com>
2023-07-26 10:41:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'C#'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    };
+    console.log(member.skills[4]);
+    console.log(member.skills[3]);
+    console.log(member.skills[6]);
+}
